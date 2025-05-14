1133e75d339f65b79eac4a1ecc6a5dd8648c8dca
Ilyas-Externe Soufiane <Ilyas-Externe.Soufiane@asmeg.org>
2025-05-14 14:13:24 +0200

    Mise à jour du workflow GitHub Actions pour utiliser allure-cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('Cypress');
+const { defineConfig } = require('cypress');
 const { allureCypress } = require('allure-cypress/reporter');
 module.exports = defineConfig({
   e2e: {
